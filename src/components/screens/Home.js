720f21efc948bad05c54e98d901ffd9fcfc45de4
Emre Yılmaz <hintlikaraemre@gmail.com>
2020-11-10 13:00:27 +0300

    home like-unlike

--- src/components/screens/Home.js
+++ src/components/screens/Home.js
@@ -1,8 +1,9 @@
-import React, { useEffect, useState } from "react";
-
+import React, { useEffect, useState ,useContext} from "react";
+import {UserContext} from "../../App"
 
 const Home = () => {
     const [data, setData] = useState([]);
+    const {state,dispatch} = useContext(UserContext)
 
     useEffect(() => {
         fetch("/allpost", {
@@ -16,18 +17,81 @@ const Home = () => {
             })
     }, [])
 
+    const likePost = (id) => {
+        
+        fetch("/like",{
+            method:"put",
+            headers: {
+                "Content-Type":"application/json",
+                "Authorization":"Bearer "+localStorage.getItem("jwt")
+            },
+            body : JSON.stringify({
+                postId: id
+            })
+        }).then(res=>res.json())
+        .then(result=> {
+
+            const newData = data.map(item=>{
+                if(item._id == result._id){
+                    return result
+                }else{
+                    return item
+                }
+            })
+
+            setData(newData)
+        }).catch(err=>{
+            console.log(err)
+        })
+    }
+
+    const unlikePost = (id) => {
+        console.log("çalışıyor")
+        fetch("/unlike",{
+            method:"put",
+            headers: {
+                "Content-Type":"application/json",
+                "Authorization":"Bearer "+localStorage.getItem("jwt")
+            },
+            body : JSON.stringify({
+                postId: id
+            })
+        }).then(res=>res.json())
+        .then(result=> {
+
+            //console.log(result)
+            const newData = data.map(item=> {
+                if(item._id==result._id){
+                    return result
+                }else {
+                    return item
+                }
+            })
+
+            setData(newData)
+        }).catch(err=>{
+            console.log(err)
+        })
+    }
+
     return (
         <div className="home">
             {
                 data.map(item => {
                     return (
-                        <div className="card home-card" key={item.id}>
+                        <div className="card home-card" key={item._id}>
                             <h5>{item.postedBy.name}</h5>
                             <div className="card-image">
                                 <img src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"></img>
                             </div>
                             <div className="card-content">
                                 <i className="material-icons" style={{ color: "red" }}>favorite</i>
+                                {item.likes.includes(state._id) 
+                                ?<i className="material-icons" style={{ color: "black" }} onClick={() => unlikePost(item._id)}>thumb_down</i>
+                                :<i className="material-icons" style={{ color: "black" }} onClick={() => likePost(item._id)}>thumb_up</i>                      
+                            }
+                                
+                                <h6>{item.likes.length}</h6>
                                 <h6>{item.title}</h6>
                                 <p>{item.body}</p>
                                 <input type="text" placeholder="add a comment" />
--- src/components/screens/Profile.js
+++ src/components/screens/Profile.js
@@ -39,7 +39,7 @@ const Profile = () => {
             <div className="gallery">
                 {myPics.map(item=> {
                     return(
-                        <img className="item" src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"></img>
+                        <img key={item._id} className="item"  src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"></img>
 
                     )
 
